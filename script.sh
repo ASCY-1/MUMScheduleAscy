c57cf385e40ce949242cb43a79c0a139735f480b
Cleave <cmasereka@gmail.com>
2017-04-14 15:48:31 -0500

    Added the shell script with all the commands needed to manage the repo

---
+++ script.sh
@@ -0,0 +1,34 @@
+function gp(){
+   git pull
+}
+
+function gb(){
+   git branch
+}
+
+function gac(){
+   git add .
+   git commit -m"$1"
+}
+
+function gacp() {
+    git add .
+    git commit -m"$1"
+    git push
+}
+
+function gs(){
+   git status;
+}
+
+function gco(){
+   git checkout $1
+}
+
+function cod(){
+   git checkout develop
+}
+
+function com(){
+   git checkout master
+}
